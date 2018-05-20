40cd97c92fdbae1d1955a1b45ec948341c0819db
Habit <habit_shine@163.com>
2018-05-21 01:07:16 +0800

    was

--- template/common/index.js
+++ template/common/index.js
@@ -1,5 +1,4 @@
-<% _.forEach(config.projects, function(project){%>import * as {{$$.convertUrl(project.name)}} from './{{project.name}}'
-<%})%>
-export {<% _.forEach(config.projects, function(project, i){ %>
+<% _.forEach(config.projects, function(project){ %>import * as {{$$.convertUrl(project.name)}} from './{{project.name}}'
+<% }) %>export {<% _.forEach(config.projects, function(project, i){ %>
   {{$$.convertUrl(project.name)}}<% if(config.projects.length - 1 !== i) { %>,<% } %><% }) %>
 }
--- template/cover/index.js
+++ template/cover/index.js
@@ -1,15 +1,13 @@
-import request from '@/util/request'
-import { convertRESTAPI } from '@/util'
-
+import instance from './instance'
+import { convertRESTAPI } from '{{$$.relative("util")}}'
 <% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
 function {{$$.convertMethod(mock)}}(opts) {
-  return request({
+  return instance({
     method: '{{mock.method}}',
     url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{mock.url}}', opts)<%} else {%> '{{mock.url}}'<% } %>,
     opts: opts
   })
 }
-
 <% }) %>export {<% _.forEach(data.mocks, function(mock, i){ %>
   {{$$.convertMethod(mock)}}<% if(data.mocks.length - 1 !== i) { %>,<% } %><% }) %>
 }
